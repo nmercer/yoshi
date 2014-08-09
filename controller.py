34739a3624712c78c8c512aaa778d3e9bbca3cf4
nmercer <nm3rc3r@gmail.com>
2014-08-09 13:15:37 -0400

    Moar

--- controller.py
+++ controller.py
@@ -1,8 +1,5 @@
-import requests
-import json
 import RPi.GPIO as GPIO
-import time
-import random
+from mopidy import Mopidy
 
 BUTTON_PIN = 25
 LIGHT_PIN = 22
@@ -11,58 +8,14 @@ LIGHT_PIN = 22
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(BUTTON_PIN, GPIO.IN)
 GPIO.setup(LIGHT_PIN, GPIO.OUT)
-#GPIO.setup(NEW_BUTTON_PIN, GPIO.IN)
-
-MOPIDY_URL = 'http://localhost:6680/mopidy/rpc'
-
-def send(method, params={}):
-    data = json.dumps({"jsonrpc": "2.0", "id": 1, "method": str(method), "params":params})
-    r = requests.post(MOPIDY_URL, data=data)
-    return r.json()
-
-def resume():
-    return send('core.playback.resume')
-
-def pause():
-    return send('core.playback.pause')
-
-def get_state():
-    return send('core.playback.get_state')['result']
-
-def get_playlists():
-    return send('core.playlists.get_playlists')['result']
-
-def get_random_playlist(playlists):
-    return random.choice(playlists)['tracks']
-
-def clear_tracklist():
-    return send('core.tracklist.clear')
-
-def add_to_tracklist(tracks):
-    params = {'tracks':tracks}
-    return send('core.tracklist.add', params=params)
-
-def get_tracklist():
-    return send('core.tracklist.get_tracks')
-
-def get_track():
-    return send('core.playback.get_current_tl_track')
-
-def play():
-    return send('core.playback.play')
-
+# GPIO.setup(NEW_BUTTON_PIN, GPIO.IN)
 
 print('Started...')
 print('Getting Playlists...')
+player = Mopidy()
+print('Started')
 
-playlists = get_playlists()
-playlist = get_random_playlist(playlists)
-
-clear_tracklist()
-add_to_tracklist(playlist)
-play()
-
-print('Ready...')
+player.play_new_playlist()
 
 prev_input = 1
 while True:
@@ -75,9 +28,9 @@ while True:
         state = get_state()
         print state
         if state == 'paused' or state == 'stopped':
-            resume()
+            player.resume()
         else:
-            pause()
+            player.pause()
             
     elif prev_input != input and input:
         print("Button Released")
---
+++ mopidy.py
@@ -0,0 +1,56 @@
+import requests
+import json
+import time
+import random
+
+MOPIDY_URL = 'http://localhost:6680/mopidy/rpc'
+
+class Mopidy(object):
+
+    def __init__(self):
+        self.playlists = self.get_playlists()
+        self.playlist = None
+
+    def send(self, method, params={}):
+        data = json.dumps({"jsonrpc": "2.0", "id": 1, "method": str(method), "params":params})
+        r = requests.post(MOPIDY_URL, data=data)
+        return r.json()
+
+    def resume(self):
+        return self.send('core.playback.resume')
+
+    def pause(self):
+        return self.send('core.playback.pause')
+
+    def get_state(self):
+        return self.send('core.playback.get_state')['result']
+
+    def get_playlists(self):
+        return self.send('core.playlists.get_playlists')['result']
+
+    # Todo - Store like last 10 playlist and don't grab any of those
+    def get_random_playlist(self):
+        return random.choice(self.playlists)['tracks']
+
+    def clear_tracklist(self):
+        return self.send('core.tracklist.clear')
+
+    def add_to_tracklist(self, tracks):
+        params = {'tracks':tracks}
+        return self.send('core.tracklist.add', params=params)
+
+    def get_tracklist(self):
+        return self.send('core.tracklist.get_tracks')
+
+    def get_track(self):
+        return self.send('core.playback.get_current_tl_track')
+
+    def play(self):
+        return self.send('core.playback.play')
+
+    def play_new_playlist(self):
+        self.clear_tracklist()
+        self.playlist = self.get_random_playlist()
+        self.add_to_tracklist(self.playlist)
+        self.play()
+
