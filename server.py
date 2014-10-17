26ad89b3fb8529072cceb602ce62513401f1138d
nmercer <nm3rc3r@gmail.com>
2014-10-16 23:20:23 -0400

    Testin

---
+++ server.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template('server.html')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80, debug=True)
